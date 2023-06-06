5caacb251a81a4615b5273bb3149be09ec8b8336
taherul77 <taherul2317@gmail.com>
2023-06-06 21:11:32 +0600

    setup

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAoIZ1pxBHjWX3iAKxPrN5-w9iJNVWahpo",
+  authDomain: "summer-camp-7067e.firebaseapp.com",
+  projectId: "summer-camp-7067e",
+  storageBucket: "summer-camp-7067e.appspot.com",
+  messagingSenderId: "566810575412",
+  appId: "1:566810575412:web:e64f2547cd7ee2aaec8061"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
