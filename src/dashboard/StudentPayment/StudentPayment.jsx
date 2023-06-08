44cc1bfa2f0ef8fd0d001fe5b7a92483b5391398
taherul77 <taherul2317@gmail.com>
2023-06-08 20:08:22 +0600

    update pay

--- src/dashboard/StudentPayment/StudentPayment.jsx
+++ src/dashboard/StudentPayment/StudentPayment.jsx
@@ -3,7 +3,29 @@
 const StudentPayment = () => {
     return (
         <div>
-            kkkk
+            <div className="overflow-x-auto">
+  <table className="table table-zebra">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+      
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
