d1e155da877a5b24d0e492a59575cc4bfe1d8d92
taherul77 <taherul2317@gmail.com>
2023-06-10 02:06:53 +0600

    update project

--- src/pages/AllInstructors/InstructorCart.jsx
+++ src/pages/AllInstructors/InstructorCart.jsx
@@ -1,33 +1,33 @@
+
+
 /* eslint-disable react/prop-types */
-const InstructorCart = ({img,instructor,price,taken}) => {
+const InstructorCart = ({ img, instructor, price, taken }) => {
   return (
     <div>
-    <div>
-<div className="max-w-xs overflow-hidden bg-white rounded-lg shadow-lg ">
-  <div className="px-4 py-2">
-    <h1 className="text-xl font-bold uppercase ">{instructor}</h1>
-  
-  </div>
+      <div>
+        <div className="max-w-xs overflow-hidden bg-white rounded-lg shadow-lg ">
+          <div className="px-4 py-2">
+            <h1 className="text-xl font-bold uppercase ">{instructor}</h1>
+          </div>
 
-  <img
-    className="object-cover w-full h-50 mt-2"
-    src={img}
-    alt="NIKE AIR"
-  />
-  <p>Class Taken : {taken}</p>
+          <img
+            className="object-cover w-full h-50 mt-2"
+            src={img}
+            alt="NIKE AIR"
+          />
+          <p>Class Taken : {taken}</p>
 
-  <div className="flex items-center justify-between px-4 py-2 ">
-    <h1 className="text-lg font-bold ">${price}</h1>
-  
-    <button className="px-2 py-1 text-xs font-semibold uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
-      See Classes
-    </button>
-  </div>
-</div>
+          <div className="flex items-center justify-between px-4 py-2 ">
+            <h1 className="text-lg font-bold ">${price}</h1>
 
-</div>
-
-</div>
+            <button className="px-2 py-1 text-xs font-semibold uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
+              See Classes
+            </button>
+          </div>
+        </div>
+      </div>
+      
+    </div>
   );
 };
 
--- src/pages/Home/Banner/Banner.jsx
+++ src/pages/Home/Banner/Banner.jsx
@@ -1,40 +1,39 @@
-import "react-responsive-carousel/lib/styles/carousel.min.css";
-import { Carousel } from 'react-responsive-carousel';
-import './Banner.css'
+import "./banner.css";
 
-
-1
 const Banner = () => {
   return (
-    <Carousel autoPlay={true} showArrows={false} showStatus={false} >
-      <div>
-        <img src="https://i.ibb.co/TcKpk1X/pngtree-fashion-blue-banner-banners-design-vector-material-picture-image-1185823.png" />
-        <p className="legend">Legend 1</p>
-        
+    <section className="">
+      <div className=" banner-bg container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
+        <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+          <p className="inline-block px-3 py-px mb-4 text-2xl font-bold  text-transparent  bg-clip-text bg-gradient-to-r from-pink-500 to-sky-600">
+            UNIQUE FASHION DESIGN COURSE
+          </p>
+          <h2 className="px-3 text-4xl font-bold text-transparent  bg-clip-text bg-gradient-to-r from-blue-500 to-sky-600">
+          Online Education from Fashion Industry Leaders
+          </h2>
+          <p className="mt-6 px-3 mb-8 text-lg sm:mb-12 text-white">
+          Discover the business of fashion and the cultural impact of the fashion industry with courses from industry leaders.
+            
+          </p>
+          <div className="flex flex-col px-3 space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="px-8 py-2 font-bold text-cyan-50 border-md rounded-md bg-pink-400  bg-gradient-to-r from-pink-500 to-sky-300"
+            >
+              Enrolled Now
+            </a>
+          </div>
+        </div>
+        <div className="flex items-center justify-center p-6 mt-8 l">
+          <img
+            src="https://i.ibb.co/f92LtQB/95d06ef7f27544514b0b7176c06478e2.jpg"
+            alt=""
+            className="object-contain h-72 sm:h-80 lg:h-50 "
+          />
+        </div>
       </div>
-      <div>
-        <img src="https://i.ibb.co/TcKpk1X/pngtree-fashion-blue-banner-banners-design-vector-material-picture-image-1185823.png" />
-        <p className="legend">Legend 1</p>
-     
-      </div>
-      <div>
-        <img src="https://i.ibb.co/TcKpk1X/pngtree-fashion-blue-banner-banners-design-vector-material-picture-image-1185823.png" />
-        <p className="legend">Legend 1</p>
-     
-      </div>
-      <div>
-        <img src="https://i.ibb.co/TcKpk1X/pngtree-fashion-blue-banner-banners-design-vector-material-picture-image-1185823.png" />
-        <p className="legend">Legend 1</p>
-     
-      </div>
-      <div>
-        <img src="https://i.ibb.co/TcKpk1X/pngtree-fashion-blue-banner-banners-design-vector-material-picture-image-1185823.png" />
-        <p className="legend">Legend 1</p>
-     
-      </div>
-      
-    
-    </Carousel>
+    </section>
   );
 };
 
